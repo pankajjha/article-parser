8f7cdc943e4f532811d845cdf56bfdb9ba0e9cf0
Pankaj Jha <pankajjha.145@gmail.com>
2022-02-28 12:20:51 +0530

    Updated Package.json for main depreciation

--- run.js
+++ run.js
@@ -5,7 +5,7 @@ const url_1 = 'https://www.newsbreak.com/news/2521982066231/uk-weather-news-live
 const url_2 = 'https://www.newsbreak.com/news/2522075079359/how-new-drills-emphasis-on-individuals-is-driving-hitting-development';
 const url_3 = 'https://www.dkpittsburghsports.com/2022/02/21/pirates-hitting-prospects-news-mlb-tucker-martin-chandler-minor-league-camp-ajs';
 
-extract(url_2).then((article) => {
+extract(url_1).then((article) => {
   console.log(article)
 }).catch((err) => {
     console.log('parse_error')
