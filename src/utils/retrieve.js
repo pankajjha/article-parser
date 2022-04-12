5cd429a13d53e9fd766640b4a47e2f7483765e57
Pankaj Jha <pankajjha.145@gmail.com>
2022-04-12 14:25:57 +0530

    Replaced Axios with undici

--- src/utils/retrieve.js
+++ src/utils/retrieve.js
@@ -1,24 +1,17 @@
 // utils -> retrieve
 
-import axios from 'axios'
-
-import logger from './logger.js'
+//import axios from 'axios'
+import logger from './logger.js';
+import { request } from 'undici';
 
 import { getRequestOptions } from '../config.js'
 
 export default async (url) => {
-  try {
-    const res = await axios.get(url, getRequestOptions())
-
-    const contentType = res.headers['content-type'] || ''
-    if (!contentType || !contentType.includes('text/html')) {
-      logger.error(`Content type must be "text/html", not "${contentType}"`)
-      return null
-    }
-
-    return res.data
-  } catch (err) {
-    logger.error(err.message || err)
-    return null
-  }
-}
+  const {
+    statusCode,
+    headers,
+    trailers,
+    body
+  } = await request(url);
+  return await body.text();
+};
