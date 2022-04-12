06f6e82f18d31e207ecf856f8ca53c68b6c46110
Pankaj Jha <pankajjha.145@gmail.com>
2022-04-12 17:15:48 +0530

    Git Push Added Needle

--- src/utils/retrieve.js
+++ src/utils/retrieve.js
@@ -1,17 +1,26 @@
 // utils -> retrieve
 
-//import axios from 'axios'
-import logger from './logger.js';
-import { request } from 'undici';
+import axios from 'axios'
+import needle from 'needle';
+
+import logger from './logger.js'
 
 import { getRequestOptions } from '../config.js'
 
 export default async (url) => {
-  const {
-    statusCode,
-    headers,
-    trailers,
-    body
-  } = await request(url);
-  return await body.text();
-};
+  try {
+    const response = await needle('get',url);
+
+    // const contentType = res.headers['content-type'] || ''
+    // if (!contentType || !contentType.includes('text/html')) {
+    //   logger.error(`Content type must be "text/html", not "${contentType}"`)
+    //   return null
+    // }
+    // console.log(response.body);
+    // let html = await res.text();
+    return response.body;
+  } catch (err) {
+    logger.error(err.message || err)
+    return null
+  }
+}
